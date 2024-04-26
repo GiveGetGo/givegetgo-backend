3fbafa38d9a61ec37b06525dd85314b6d0b8ff64
Andy Yang <andyyang@pal-nat186-54-162.itap.purdue.edu>
2024-04-26 16:07:55 -0400

    feat: finish bid endpoint

--- servers/bid/utils/bid_utils.go
+++ servers/bid/utils/bid_utils.go
@@ -7,9 +7,15 @@ import (
 )
 
 type IBidUtils interface {
-	GetbidByID(bidID uint) (schema.Bid, error)
-	Addbid(bid schema.Bid) (schema.Bid, error)
+	GetBidBypostID(bidID uint) ([]schema.Bid, error)
+	AddBid(bid schema.Bid) (schema.Bid, error)
+	GetBidBybidID(bidID uint) ([]schema.Bid, error)
+	DeleteBid(bidID uint) error
+    UpdateBidDescription(bidID uint, description string) error
 }
+
+// Ensure PostUtils implements IPostUtils
+var _ IBidUtils = (*BidUtils)(nil)
 
 type BidUtils struct {
 	DB          db.Database
@@ -24,15 +30,14 @@ func NewBidUtils(DB db.Database, redisClient middleware.RedisClientInterface) *B
 	}
 }
 
-// func GetBidByID retrieves a bid by its ID
-func (bu *BidUtils) GetBidByID(bidID uint) (schema.Bid, error) {
-	var bid schema.Bid
-	err := bu.DB.First(&bid, bidID).Error
-	if err != nil {
-		return schema.Bid{}, err
-	}
-
-	return bid, nil
+// func GetBidBypostID retrieves a bid by its postID
+func (bu *BidUtils) GetBidBypostID(postID uint) ([]schema.Bid, error) {
+    var bids []schema.Bid
+    err := bu.DB.Where("post_id = ?", postID).Find(&bids).Error
+    if err != nil {
+        return nil, err
+    }
+    return bids, nil
 }
 
 // func Addbid adds a bid to the database
@@ -43,4 +48,45 @@ func (bu *BidUtils) AddBid(bid schema.Bid) (schema.Bid, error) {
 	}
 
 	return bid, nil
+}
+
+// func GetBidBypostID retrieves a bid by its bidID
+func (bu *BidUtils) GetBidBybidID(bidID uint) ([]schema.Bid, error) {
+    var bids []schema.Bid
+    err := bu.DB.Where("bid_id = ?", bidID).Find(&bids).Error
+    if err != nil {
+        return nil, err
+    }
+    return bids, nil
+}
+
+// DeletePost deletes a post from the database by its ID.
+func (pu *BidUtils) DeleteBid(bidID uint) error {
+    // Attempt to first fetch the post to ensure it exists.
+    var bid schema.Bid
+    result := pu.DB.First(&bid, bidID)
+    if result.Error != nil {
+        return result.Error  // Return the error (e.g., not found)
+    }
+
+    // If the post exists, proceed to delete it.
+    if err := pu.DB.Delete(&bid).Error; err != nil {
+        return err  // Return any error that occurs during the delete operation.
+    }
+
+    return nil  // Return nil if the delete operation is successful.
+}
+
+// UpdateBidDescription updates the description of a bid identified by bidID
+func (bu *BidUtils) UpdateBidDescription(bidID uint, description string) error {
+    // Find the bid by ID
+    var bid schema.Bid
+    result := bu.DB.First(&bid, "bid_id = ?", bidID)
+    if result.Error != nil {
+        return result.Error  // If not found or other DB error
+    }
+
+    // Update the bid's description
+    bid.BidDescription = description
+    return bu.DB.Save(&bid).Error
 }
